4fe67da35f1067525b2ca7a54f7dd86a4bdfea81
Kamilla <kamilla.sthefany@outlook.com>
2023-03-06 09:53:37 -0300

    Ainda não carrega dentro da rota principal

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './dashboard/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./dashboard/dashboard.module').then(module => module.DashboardModule)
+  // },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
+  exports:[DashboardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
